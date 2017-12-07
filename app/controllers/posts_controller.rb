bad1bdbf4eb7b45332b34638311511b023dcca2a
lk <lucaskeys@gmail.com>
2017-12-07 14:07:36 -0800

    Add Post CRUD functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,22 @@
 class PostsController < ApplicationController
   def index
   end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:date, :rationale))
+    @post.save
+    redirect_to @post
+  end
+
+  def destroy
+
+  end
 end
